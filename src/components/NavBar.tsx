98bbd54a1fa36b477c311c812dd6dbbe0a89ce8d
Atharva Swami <113084064+swami-hai-ham@users.noreply.github.com>
2024-03-09 17:11:24 +0000

    shadcn

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='bg-yellow-500 text-stone-900 font-mono font-bold uppercase p-4 flex-1'>
+      hello
+    </div>
+  )
+}
+
+export default NavBar
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Todo from '../components/Todo'
 
 const Dashboard = () => {
   return (
-    <div className=''>
-      
+    <div className='text-white bg-slate-900'>
+      <div className='bg-black text-white flex'>
+        <NavBar></NavBar>
+      </div>
+        <div className='grid grid-cols-2'>
+          <div className='bg-stone-900 h-screen border-r-2 border-stone-800 p-3'>
+            <Todo></Todo>
+          </div>
+          <div className='bg-stone-900 h-screen border-l-2 border-stone-800 p-3'> hello</div>
+        </div>
     </div>
   )
 }
